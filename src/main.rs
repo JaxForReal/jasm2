0e9eca70b9c29a90ca5adbffbb03362c9ba02dbf
JaxForReal <jackpribis@gmail.com>
2016-06-25 09:54:17 +1200

    fix parser grammar (remove deleted references)

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,7 @@ pub mod parser;
 pub mod vm;
 
 fn main() {
-    println!("{:?}",
-             parser::try_parse("ret;")
-                 .unwrap());
-
-
     let program = parser::try_parse("ret;").unwrap();
     let mut machine = vm::Vm::new();
-    machine.exec(program);
+    machine.exec(&program);
 }
