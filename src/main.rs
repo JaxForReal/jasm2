e128eb58e031c55a4abd0475cc0815fd46b2a52a
JaxForReal <jackpribis@gmail.com>
2016-06-26 10:11:38 +1200

    read program from file

--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,18 @@ mod tests;
 pub mod parser;
 pub mod vm;
 
-static PROGRAM: &'static str = "
-12 -> 1;
-data 5 8 99 456 2 -> 10;
-@@1 -> 0;
-syscall print;
-";
+use std::env;
+use std::fs::File;
+use std::io::Read;
 
 fn main() {
-    let parsed_program = parser::try_parse(PROGRAM).unwrap();
-    println!("{:?}", parsed_program);
-    let mut machine = vm::Vm::new(&parsed_program);
-    machine.exec();
+    let filename = env::args().nth(1).expect("No input file");
+    let mut file = File::open(filename).expect("could not open file");
+
+    let mut program = String::new();
+    file.read_to_string(&mut program).expect("could not read file");
+
+    let parsed_program = parser::try_parse(&program).expect("could not parse program");
+    
+    vm::Vm::new(&parsed_program).exec();
 }
