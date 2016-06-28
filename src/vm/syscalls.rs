e4e68055e5bd6287e424f66917085436a5d76085
JaxForReal <jackpribis@gmail.com>
2016-06-29 06:26:05 +1200

    add print_char, read, read_char, read_string syscalls

--- src/vm/syscalls.rs
+++ src/vm/syscalls.rs
@@ -1,5 +1,5 @@
 use super::Vm;
-use std::io::Write;
+use std::io::{self, Write};
 
 
 // implement all syscalls of the Vm
@@ -7,7 +7,10 @@ impl<'a, TOut: Write> Vm<'a, TOut> {
     pub fn syscall(&mut self, name: &str) {
         match name {
             "print" => self.print(),
-            "print_ascii" => self.print_ascii(),
+            "print_char" => self.print_char(),
+            "read" => self.read(),
+            "read_string" => self.read_string(),
+            "read_char" => self.read_char(),
             _ => panic!("unknown syscall"),
         }
     }
@@ -23,9 +26,37 @@ impl<'a, TOut: Write> Vm<'a, TOut> {
     }
 
     // print the value in address 0 as an ascii char
-    fn print_ascii(&mut self) {
+    fn print_char(&mut self) {
         let value = self.get_ram(0);
         // write the value directly to out, because stdout formats as a char
         self.output.write(&[value as u8]).expect("couldnt write to output");
+    }
+
+    // read a single value into address 0
+    //returns 0 if the value coild not e parsed.
+    fn read(&mut self) {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("couldnt read from stdin");
+        let value = input.trim().parse::<u32>().unwrap_or(0);
+        self.set_ram(0, value);
+    }
+
+    //read a string of utf8 chars starting at the pointer in call #0
+    fn read_string(&mut self) {
+        let save_pointer = self.get_ram(0);
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("oculdnt read from stdin");
+        for (index, chr) in input.chars().enumerate() {
+            self.set_ram(save_pointer as usize + index, chr as u32);
+        }
+    }
+
+    //reads a single utf-8 char to memory cell 0
+    fn read_char(&mut self) {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("couldnt read from stdin");
+        let chr = input.chars().nth(0).unwrap_or(0 as char) as u32;
+        self.set_ram(0, chr);
     }
 }
