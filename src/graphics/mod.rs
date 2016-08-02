32105aae301fd4c267153ffcb7efa2fced429822
JaxForReal <jackpribis@gmail.com>
2016-08-03 00:59:57 +1200

    add buffered drawing for screen output

--- src/graphics/mod.rs
+++ src/graphics/mod.rs
@@ -10,8 +10,17 @@ use self::sdl2::pixels;
 use self::sdl2::rect;
 use self::sdl2::event::Event;
 
+// the width and height of the buffer (size = BUFFER_WIDTH * BUFFER_WIDTH)
+const BUFFER_WIDTH: usize = 20;
+// width in pixels in which one cell is rendered
+const CELL_SIZE: usize = 20;
+
+// colors that are drawn to the screen based on buffer values
+static TRUE_COLOR: pixels::Color = pixels::Color::RGB(0, 0, 0);
+static FALSE_COLOR: pixels::Color = pixels::Color::RGB(255, 255, 255);
+
 pub struct MySdl<'a> {
-    pub screen_buffer: [bool; 400],
+    pub screen_buffer: [bool; BUFFER_WIDTH * BUFFER_WIDTH],
     renderer: render::Renderer<'a>,
     event_pump: sdl2::EventPump,
     pub timer: sdl2::TimerSubsystem,
@@ -21,7 +30,10 @@ impl<'a> MySdl<'a> {
     pub fn new() -> Self {
         let sdl_context = sdl2::init().unwrap();
         let video_subsystem = sdl_context.video().unwrap();
-        let window = video::WindowBuilder::new(&video_subsystem, "Output", 400, 400)
+        let window = video::WindowBuilder::new(&video_subsystem,
+                                               "Output",
+                                               (BUFFER_WIDTH * CELL_SIZE) as u32,
+                                               (BUFFER_WIDTH * CELL_SIZE) as u32)
             .position_centered()
             .build()
             .unwrap();
@@ -35,8 +47,22 @@ impl<'a> MySdl<'a> {
     }
 
     pub fn render(&mut self) {
-        self.renderer.set_draw_color(pixels::Color::RGB(255, 0, 0));
-        self.renderer.fill_rect(rect::Rect::new(10, 10, 20, 20));
+        for iter_y in 0..BUFFER_WIDTH {
+            for iter_x in 0..BUFFER_WIDTH {
+                let cell_value = self.screen_buffer[iter_y * BUFFER_WIDTH + iter_x];
+                let color = if cell_value {
+                    TRUE_COLOR
+                } else {
+                    FALSE_COLOR
+                };
+
+                self.renderer.set_draw_color(color);
+                self.renderer.fill_rect(rect::Rect::new((iter_x * CELL_SIZE) as i32,
+                                                        (iter_y * CELL_SIZE) as i32,
+                                                        CELL_SIZE as u32,
+                                                        CELL_SIZE as u32));
+            }
+        }
         self.renderer.present();
     }
 
