b09a1ace06e0445a971cd73d2c934ca5f421aa74
JaxForReal <jackpribis@gmail.com>
2016-08-14 02:02:13 +1200

    more advanced command line args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 #![feature(plugin)]
 #![plugin(peg_syntax_ext)]
 
+extern crate clap;
+
 #[cfg(test)]
 mod test;
 
@@ -13,22 +15,61 @@ use std::env;
 use std::fs::File;
 use std::io::{self, Read};
 use std::path::Path;
+use std::process;
+
+use clap::{App, Arg};
 
 fn main() {
-    // skip the 0th arg, because it is inherently the path of the binary (useless here)
-    let path_string = &env::args().nth(1).expect("No input file");
-    let file_path = Path::new(path_string);
-    let mut file = File::open(file_path).expect("could not open file");
+    let matches = App::new("JASM interpreter")
+        .author("Liam Pribis <liampribis@gmail.com>")
+        .arg(Arg::with_name("filename")
+            .index(1)
+            .help("The file to run.")
+            .required_unless("string"))
+        .arg(Arg::with_name("string")
+            .short("s")
+            .long("string")
+            .takes_value(true)
+            .conflicts_with("filename")
+            .help("Runs the string as a JASM file"))
+        .arg(Arg::with_name("emit_type")
+            .short("e")
+            .long("emit")
+            .takes_value(true)
+            .possible_values(&["preprocessed", "parsed"]))
+        .get_matches();
+
 
     let mut program = String::new();
-    file.read_to_string(&mut program).expect("could not read file");
+    let current_pathbuf = env::current_dir().unwrap();
+    let file_path = if matches.is_present("string") {
+        program += matches.value_of("string").unwrap();
+        current_pathbuf.as_path()
+    } else {
+        // if a file was used instead of a string
+        let file_path = Path::new(matches.value_of("filename").unwrap());
+        let mut file = File::open(file_path).expect("could not open file");
+        file.read_to_string(&mut program).expect("could not read file");
+        file_path
+    };
 
     program = preprocessor::preprocess(&program, file_path);
-    // println!("{}", program);
+
+    if matches.value_of("emit_type") == Some("preprocessed") {
+        println!("{}", program);
+        process::exit(0);
+    }
+
     let parsed_program = match parser::try_parse(&program) {
         Ok(prog) => prog,
         Err(err) => panic!("Error parsing: {}", err),
     };
+
+    if matches.value_of("emit_type") == Some("parsed") {
+        for command in &parsed_program {
+            println!("{:?}", command);
+        }
+    }
 
     // println!("{:?}", parsed_program);
     let stdout = io::stdout();
